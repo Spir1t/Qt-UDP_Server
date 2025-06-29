e8fa5db9e2928d35907ab10101f81660ae8fb4b1
Spir1t <rinchinov.slava@mail.ru>
2025-06-29 15:51:10 +0300

    Clean up

--- udp_server.cpp
+++
@@ -1,9 +0,0 @@
-#include <QUdpSocket>
-#include <memory>
-#include "udp_server.h"
-
-UDP_Server::UDP_Server()
-{
-    Socket = std::make_unique<QUdpSocket>();
-    Socket->bind(QHostAddress::LocalHost, 1234);
-}
